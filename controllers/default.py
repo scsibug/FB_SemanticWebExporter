c0fb71e9b12e43fffc3f65f1ed93abad0767cd5a
Greg Heartsfield <scsibug@imap.cc>
2010-10-30 01:19:20 +0000

    Fixed format strings based on rdflib 3.0 updates.

--- controllers/default.py
+++ controllers/default.py
@@ -11,7 +11,6 @@ from rdflib import Namespace
 from rdflib import Literal
 from rdflib import BNode
 from rdflib import URIRef
-from rdflib.TripleStore import TripleStore
 from os import urandom
 
 foafp = "http://xmlns.com/foaf/0.1/"
@@ -19,8 +18,8 @@ rdfs = "http://www.w3.org/2000/01/rdf-schema#"
 sioc = "http://rdfs.org/sioc/ns#"
 dc = "http://purl.org/dc/elements/1.1/"
 type = rdflib.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#subject')
-allowed_formats = ['rdf', 'n3', 'nt', 'turtle', 'pretty-xml']
-default_format = 'rdf'
+allowed_formats = ['xml', 'n3', 'nt', 'turtle', 'pretty-xml']
+default_format = 'xml'
 
 # prefix for saving facebook sessions: append uid, lookup, and get facebook session.
 fb_cache_prefix="facebook-"
