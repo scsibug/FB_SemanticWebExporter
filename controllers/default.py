c477b308a31159a4b236f752cf4fb5d968316baf
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2011-01-02 17:41:17 -0600

    Use the correct property, rdf:type, not rdf:subject, for instances of a class.

--- controllers/default.py
+++ controllers/default.py
@@ -17,7 +17,7 @@ foafp = "http://xmlns.com/foaf/0.1/"
 rdfs = "http://www.w3.org/2000/01/rdf-schema#"
 sioc = "http://rdfs.org/sioc/ns#"
 dc = "http://purl.org/dc/elements/1.1/"
-type = rdflib.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#subject')
+type = rdflib.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
 allowed_formats = ['xml', 'n3', 'nt', 'turtle', 'pretty-xml']
 default_format = 'xml'
 
